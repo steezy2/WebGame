24b5711569f9c16a33a868ed2a79ac04e8fb6093
Nick Klonne <nklonne@mines.edu>
2020-10-09 13:36:05 -0500

    road lines

--- nocopnostop.js
+++ nocopnostop.js
@@ -3,10 +3,10 @@
  * Declarations 
  * 
  */
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var canvasH = canvas.height;
-var canvasW = canvas.width;
+var canvas;
+var ctx;
+var canvasH;
+var canvasW;
 
 
 /* TODO
@@ -15,6 +15,12 @@ var canvasW = canvas.width;
  * 
  */
 window.onload = function() {
+	// Get Elements
+	canvas = document.getElementById("myCanvas");
+	ctx = canvas.getContext("2d");
+	canvasH = canvas.height;
+	canvasW = canvas.width;
+
 	// Draw Background
 	drawBackground();
 	// Begin Cars Crossing
@@ -28,18 +34,48 @@ window.onload = function() {
  * 
  */
 function drawBackground() {
-	// Pavement
-	ctx.fillStyle = "#BBBBCC";
-	var rdStart = (canvasW / 2) - (canvasW / 8);
-	var rdW = canvasW / 4;
-	ctx.fillRect(rdStart, 0, rdW, canvasH);
-	rdStart = (canvasH / 2) - (canvasH / 8);
-	rdW = canvasH / 4;
-	ctx.fillRect(0, rdStart, canvasW, rdW);
+	// ***BG Grass***
+	ctx.fillStyle = "#00FF99";
+	ctx.fillRect(0, 0, canvasW, canvasH);
 
-	// Yellow Lines
-	// Crosswalk
-	// Fun Extras
+	// ***Pavement***
+	ctx.fillStyle = "#BBBBCC";
+	// vertical
+	var rStart = (canvasW / 2) - (canvasW / 8);
+	var rW = canvasW / 4;
+	ctx.fillRect(rStart, 0, rW, canvasH);
+	// horizontal
+	rStart = (canvasH / 2) - (canvasH / 8);
+	rW = canvasH / 4;
+	ctx.fillRect(0, rStart, canvasW, rW);
+
+	// ***Yellow Lines***
+	ctx.fillStyle = "#FFFF00";
+	// vertical
+	rW = 4;
+	rStart = (canvasW / 2) - (rW / 2);
+	var rL = 16;
+	for (var curY = 0; curY <= canvasH - rL; curY += 2*rL) {
+		ctx.fillRect(rStart, curY, rW, rL);
+	}
+	// horizontal
+	rStart = (canvasH / 2) - (rW / 2);
+	for (var curX = 0; curX <= canvasW - rL; curX += 2*rL) {
+		ctx.fillRect(curX, rStart, rL, rW);
+	}
+	
+	// ***Crosswalk***
+	ctx.fillStyle = "#FFFFFF";
+	// vertical
+	rW = 8;
+	rL = 30;
+	rStart = (canvasH / 2) - (canvasH / 8) - rL;
+	for (var x = (canvasW / 2) - (canvasW / 8); x <= (canvasW / 2) + (canvasW / 8) - rW; x += 2*rW) {
+		ctx.fillRect(x, rStart, rW, rL);
+		ctx.fillRect(x, (canvasH / 2 + canvasH / 8), rW, rL);
+	}
+
+	// ***Fun Extras***
 }
 
 
